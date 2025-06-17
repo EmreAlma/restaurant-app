0c87a7ab301712c7d1f7addfb8c69927b1e9dc73
Emre Alma <emre@iMac-von-Emre.local>
2025-06-17 19:32:10 +0200

    Backend and frontend updates, add data.sql and API connection

---
+++ backend/src/main/resources/data.sql
@@ -0,0 +1,7 @@
+INSERT INTO menu_item (name, description, price, image) VALUES 
+('Beef Kebab Platter', 'A platter of perfection, traditional Turkish kebabs.', 15.99, '/images/menu-1.png'),
+('Fish & Chips', 'A British favoured cuisine, deep fried cod fish with crispy chips.', 14.99, '/images/menu-2.png'),
+('Beef Shawarma', 'A famous Middle Eastern street food, with perfectly sliced beef.', 6.99, '/images/menu-3.png'),
+('Oreo Cheesecake', 'A sweet cheesy dessert with black forest oreo creating a perfect symphony.', 7.99, '/images/menu-4.png'),
+('The Tandoori Platter', 'A chicken tandoori platter with traditional flavours.', 13.99, '/images/menu-5.png'),
+('Assorted Fruit Salad', 'An array of fruits combining the fresh and juicy flavors of an assorted mix.', 5.99, '/images/menu-6.png');
